0e884b08b63602acd82706cb6612dd5a03d7d5e5
Adam <adamwatters11@gmail.com>
2016-10-16 17:55:06 -0400

    refactored functinos, added input format to readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-tic-tac-toe without side effects or loops
+terminal tic-tac-toe without side effects or loops.
 
+--> npm install
 --> node game.js
+
+enter moves as space seperated coordinated, eg 0,0
--- game.js
+++ game.js
@@ -1,128 +1,24 @@
 var util = require('util');
 var chalk = require('chalk');
 
-var makeNewBoard = function(board) {
-	return board.map(function(c){
-		return c.slice();
+var startState = {
+	board: [['-','-','-'],['-','-','-'],['-','-','-']],
+	turn: 'X',
+	winner: 'none'
+}
+
+game(startState);
+
+function game(state) {
+	draw(state);
+	if (state.winner === 'none'){
+		takeInput(state, function(input){
+			game(changeTurn(checkForGameEnd(runMove(state, input))));
 		});
-}
-
-var rotateBoard = function(board) {
-	return [].concat.apply([], board).reduce(function(p, c, i){
-		if (p[i % board.length]) {
-			p[i % board.length].push(c);
-			return p;
-		}
-		p[i % board.length] = [c];
-		return p;
-	}, []);
-};
-
-var getLeftDiagnol = function(board) {
-		return board.reduce(function(p, c, i){
-			p.push(c[i]);
-			return p;
-	}, []);
-}
-
-var getRightDiagnol = function(board) {
-	return board.reduce(function(p, c, i){
-			p.push(c[board.length - 1 - i]);
-			return p;
-	}, []);
-};
-
-var checkRowForWin = function(player, row) {
-	return row.every(function(val){return val === player})
-};
-
-var checkRowsForWin = function(board, turn) {
-	return (board.some(function(row){
-		return checkRowForWin(turn, row);
-	})) 
-};
-
-var checkRowForEmpties = function(row) {
-	return row.some(function(val){return val === '-'})
-};
-
-var checkBoardForEmpties = function(board) {
-	return (board.some(function(row){
-		return checkRowForEmpties(row);
-	}))
-};
-
-var handleWin = function(state) {
-	return {
-		board: makeNewBoard(state.board),
-		turn: state.turn,
-		winner: state.turn
 	};
 };
 
-var handleDraw = function (state) {
-	return {
-		board: makeNewBoard(state.board),
-		turn: state.turn,
-		winner: 'draw'
-	};	
-};
-
-var handleNoEnd = function (state) {
-	return {
-		board: makeNewBoard(state.board),
-		turn: state.turn,
-		winner: 'none'
-	};
-};
-
-var rowWin = function(state) {
-	return checkRowsForWin(state.board, state.turn);
-};
- 
-var columnWin = function(state) {
-	return checkRowsForWin(rotateBoard(state.board), state.turn);
-}
-
-var diagonalWin = function(state) {
-	return checkRowForWin(state.turn, getLeftDiagnol(state.board)) ||
-	checkRowForWin(state.turn, getRightDiagnol(state.board));
-};
-
-var checkForGameEnd = function(state){
-	if (rowWin(state) || (columnWin(state)) || (diagonalWin(state))){
-		return handleWin(state);
-	} else if (!checkBoardForEmpties(state.board)) {
-		return handleDraw(state);
-	}
-	return handleNoEnd(state);
-};
-
-var genMove = function(state, input) {
-	var newBoard = makeNewBoard(state.board);
-	var x = input[0];
-	var y = input[1];
-	newBoard[x][y] = state.turn;
-	return newBoard;
-};
-
-var runMove = function(state, input){
-	return {
-		board: genMove(state, input),
-		turn: state.turn,
-		winner: state.winner
-	}
-};
-
-var changeTurn = function	(state) {
-	return {
-		board: makeNewBoard(state.board),
-		turn: state.turn === 'X' ? 'O' : 'X',
-		winner: state.winner
-	}
-};
-
-var draw = function(state){
+function draw(state) {
 	state.board.map(function(row){console.log(chalk.bold.yellow(row))});
 	console.log('');
 	if (state.winner === 'X' || state.winner === 'O') {
@@ -135,23 +31,7 @@ var draw = function(state){
 	}
 };
 
-var parseInput = function(text) {
-	return text.replace(/(\r\n|\n|\r)/gm,"").split(',').reverse();
-}
-
-var textIsValid = function(input, state) {
-	return ((input.length === 2)
-		&&(input[0] >= 0)
-		&&(input[0] <= state.board.length - 1)
-		&&(input[1] >= 0)
-		&&(input[1] <= state.board.length - 1))
-};
-
-var noCollision = function(input, state) {
-	return state.board[input[0]][input[1]] === '-';
-}
-
-var takeInput = function(state, gameCallBack) {
+function takeInput(state, gameCallBack) {
 	console.log(chalk.bold.white(state.turn + "'s turn\n"));
 	process.stdin.resume();
   	process.stdin.setEncoding('utf8');
@@ -166,22 +46,144 @@ var takeInput = function(state, gameCallBack) {
   	});
 };
 
-var game = function(state){
-	draw(state);
-	if (state.winner === 'none'){
-		takeInput(state, function(input){
-			game(changeTurn(checkForGameEnd(runMove(state, input))));
+function makeNewBoard(board) {
+	return board.map(function(c){
+		return c.slice();
 		});
+}
+
+function rotateBoard(board) {
+	return [].concat.apply([], board).reduce(function(p, c, i){
+		if (p[i % board.length]) {
+			p[i % board.length].push(c);
+			return p;
+		}
+		p[i % board.length] = [c];
+		return p;
+	}, []);
+};
+
+function getLeftDiagnol(board) {
+		return board.reduce(function(p, c, i){
+			p.push(c[i]);
+			return p;
+	}, []);
+}
+
+function getRightDiagnol(board) {
+	return board.reduce(function(p, c, i){
+			p.push(c[board.length - 1 - i]);
+			return p;
+	}, []);
+};
+
+function checkRowForWin(player, row) {
+	return row.every(function(val){return val === player})
+};
+
+function checkRowsForWin(board, turn) {
+	return (board.some(function(row){
+		return checkRowForWin(turn, row);
+	})) 
+};
+
+function checkRowForEmpties(row) {
+	return row.some(function(val){return val === '-'})
+};
+
+function checkBoardForEmpties(board) {
+	return (board.some(function(row){
+		return checkRowForEmpties(row);
+	}))
+};
+
+function handleWin(state) {
+	return {
+		board: makeNewBoard(state.board),
+		turn: state.turn,
+		winner: state.turn
 	};
 };
 
-var startState = {
-	board: [['-','-','-'],['-','-','-'],['-','-','-']],
-	turn: 'X',
-	winner: 'none'
+function handleDraw (state) {
+	return {
+		board: makeNewBoard(state.board),
+		turn: state.turn,
+		winner: 'draw'
+	};	
+};
+
+function handleNoEnd (state) {
+	return {
+		board: makeNewBoard(state.board),
+		turn: state.turn,
+		winner: 'none'
+	};
+};
+
+function rowWin(state) {
+	return checkRowsForWin(state.board, state.turn);
+};
+ 
+function columnWin(state) {
+	return checkRowsForWin(rotateBoard(state.board), state.turn);
 }
 
-game(startState);
+function diagonalWin(state) {
+	return checkRowForWin(state.turn, getLeftDiagnol(state.board)) ||
+	checkRowForWin(state.turn, getRightDiagnol(state.board));
+};
+
+function checkForGameEnd(state){
+	if (rowWin(state) || (columnWin(state)) || (diagonalWin(state))){
+		return handleWin(state);
+	} else if (!checkBoardForEmpties(state.board)) {
+		return handleDraw(state);
+	}
+	return handleNoEnd(state);
+};
+
+function genMove(state, input) {
+	var newBoard = makeNewBoard(state.board);
+	var x = input[0];
+	var y = input[1];
+	newBoard[x][y] = state.turn;
+	return newBoard;
+};
+
+function runMove(state, input){
+	return {
+		board: genMove(state, input),
+		turn: state.turn,
+		winner: state.winner
+	}
+};
+
+function changeTurn	(state) {
+	return {
+		board: makeNewBoard(state.board),
+		turn: state.turn === 'X' ? 'O' : 'X',
+		winner: state.winner
+	}
+};
+
+function parseInput(text) {
+	return text.replace(/(\r\n|\n|\r)/gm,"").split(',').reverse();
+}
+
+function textIsValid(input, state) {
+	return ((input.length === 2)
+		&&(input[0] >= 0)
+		&&(input[0] <= state.board.length - 1)
+		&&(input[1] >= 0)
+		&&(input[1] <= state.board.length - 1))
+};
+
+function noCollision(input, state) {
+	return state.board[input[0]][input[1]] === '-';
+}
+
+
 
 
 
